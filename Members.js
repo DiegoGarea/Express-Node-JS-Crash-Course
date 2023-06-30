67de0361eadb4b410e7e4ffc010c725b8e74f9d9
Diego G <d.g.g.87@hotmail.com>
2023-06-30 02:07:20 -0600

    express node course

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 0,
+    name: 'Shannon',
+    email: 'Shannon@mail.com',
+    status: 'activo',
+  },
+  {
+    id: 1,
+    name: 'Juan',
+    email: 'Juan@mail.com',
+    status: 'activo',
+  },
+  {
+    id: 2,
+    name: 'Pablo',
+    email: 'Pablo@mail.com',
+    status: 'pasivo',
+  },
+  {
+    id: 3,
+    name: 'Ferguzman',
+    email: 'Ferguzman@mail.com',
+    status: 'activo',
+  },
+];
+
+module.exports = members;
